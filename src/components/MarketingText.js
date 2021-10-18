7093f5ac00e1332d8406dbfb7a36a0ffc1622917
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2021-10-18 20:07:52 +0530

    login page second commit

---
+++ src/components/MarketingText.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MarketingText extends React.Component{
+    render(){
+        return (
+            <div className='MarketingText'>
+                <h2 id='HeadingMarketingText'>Build your own Chatbot in minutes!</h2>
+                <p className='TextMarketingText'>Take your business to next level by creating a AI generated chatbot in minutes without writing a single line of code.</p>
+                <p className='TextMarketingText'>With our bot builder you can easily integrate you business application to a chatbot using APIs and let your business grow.</p>
+            </div>
+        );
+    }
+}
+
+export default MarketingText;
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
-
+import Container from './Container';
 
 class StartPage extends React.Component{
     render() {
         return (
             <div>
                 <Navbar></Navbar>
+                <Container></Container>
             </div>
         );
     }
