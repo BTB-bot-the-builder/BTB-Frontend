93d3698443d5a613d982bc466bddfb9b40586fe9
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2021-10-18 19:05:20 +0530

    login page commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import StartPage from './components/StartPage';
+
+
+
+
+ReactDOM.render(<StartPage/> , document.getElementById("app"));
+
+// <img src = 'https://www.insegment.com/blog/wp-content/uploads/2020/11/chatbot-marketing.gif'></img>
---
+++ src/components/StartPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+class StartPage extends React.Component{
+    render() {
+        return (
+            <div>
+                <Navbar></Navbar>
+            </div>
+        );
+    }
+}
+
+export default StartPage;
